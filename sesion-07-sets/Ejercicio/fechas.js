73cb1e28046e8963cd27a125807477dd3d130c88
Mar <maria.carranzavaldes@gmail.com>
2023-02-21 11:53:35 +0100

    ejercicio-07-3

---
+++ sesion-07-sets/Ejercicio/fechas.js
@@ -0,0 +1,11 @@
+const fechaActual = new Date()
+
+const fechaNacimiento = new Date ('May 28, 1988')
+
+
+const compara = fechaActual > fechaNacimiento
+
+const diaNacimiento = fechaNacimiento.getDate()
+const mesNacimiento = fechaNacimiento.getMonth()
+const anioNacimiento = fechaNacimiento.getFullYear()
+
